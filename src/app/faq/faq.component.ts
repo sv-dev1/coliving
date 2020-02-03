f3e6a2d6cc8bc0a989088f1fd4515440fbc81482
d dev <dhiraj.dev@kindlebit.com>
2020-02-03 18:47:21 +0530

    changes

--- src/app/faq/faq.component.ts
+++ src/app/faq/faq.component.ts
@@ -6,21 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./faq.component.css']
 })
 export class FaqComponent implements OnInit {
- 
- classList: any;
- nextElementSibling: any;
- currentYear: any ;
+
+  classList: any;
+  nextElementSibling: any;
+  currentYear: any ;
+  session_key: boolean = false;
+  
   constructor() { }
 
   ngOnInit() {
-     var d = new Date();
-     this.currentYear = d.getFullYear();
-  	 const items = document.querySelectorAll(".accordion a");
-     document.body.scrollTop = 0;
+    var d = new Date();
+    this.currentYear = d.getFullYear();
+    const items = document.querySelectorAll(".accordion a");
+    document.body.scrollTop = 0;
+
+    if(sessionStorage.getItem("auth_token") != undefined){
+      this.session_key = true;
+    } 
   }
 
   toggleAccordion(){
-	  this.classList.toggle('active');
-	  this.nextElementSibling.classList.toggle('active');
-	}
+    this.classList.toggle('active');
+    this.nextElementSibling.classList.toggle('active');
+  }
 } 
